d2a483c7e58bc6cb623487ae48edf0ab22927341
sparalkar1990 <sourabh.paralkar1990@gmail.com>
2026-07-27 19:29:32 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -39,6 +39,14 @@ resource "aws_security_group" "ec2_sg" {
   }
 
   ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "HTTPS"
     from_port   = 443
     to_port     = 443
@@ -47,9 +55,10 @@ resource "aws_security_group" "ec2_sg" {
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
