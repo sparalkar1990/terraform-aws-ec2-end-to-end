027d8f91ba018b21136f2d904e50c4de16e102d7
sparalkar1990 <sourabh.paralkar1990@gmail.com>
2026-07-27 19:50:28 +0530

    backend.tf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,12 @@
 terraform {
 
   backend "s3" {
-    bucket       = "sparalkar1990-tf-backend"
-    key          = "aws-ec2-end-to-end/terraform.tfstate"
-    region       = "ap-south-1"
-    use_lockfile = "terraform-state-lock"
-    encrypt      = true
+    bucket         = "sparalkar1990-tf-backend"
+    key            = "aws-ec2-end-to-end/terraform.tfstate"
+    region         = "ap-south-1"
+    use_lockfile   = true
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
   }
 
 }
